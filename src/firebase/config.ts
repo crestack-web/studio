16d9f9a7fbbe75ec5e504da4cee7472c9721e9f0
Crestack Company <crestack@gmail.com>
2026-01-18 13:33:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4153083834-75ccb",
+  "appId": "1:161519723725:web:cceb63b33686f338dcb584",
+  "apiKey": "AIzaSyDId2AjKeFRPYYe6EnvE1RUwlNGVQZzx54",
+  "authDomain": "studio-4153083834-75ccb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "161519723725"
+};
