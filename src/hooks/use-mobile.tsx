5e02b80b28fd271a60c86280525da6c79e8897e9
Crestack Company <crestack@gmail.com>
2026-01-22 08:04:43 +0000

    correct the mobile breakpoint mockup is breaking and not showoing  (_for

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,26 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
 
-  return !!isMobile
+    const checkDevice = () => {
+        setIsMobile(mql.matches);
+    };
+
+    // Check on mount
+    checkDevice();
+
+    // Listen for changes
+    mql.addEventListener("change", checkDevice);
+
+    // Cleanup
+    return () => {
+      mql.removeEventListener("change", checkDevice);
+    };
+  }, []);
+
+  return isMobile;
 }
