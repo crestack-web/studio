1f0f0aa5ab4d6f24355e023a3d85a6ae248a1d4e
Crestack Company <crestack@gmail.com>
2026-02-01 11:27:12 +0000

    Step 1: Clean index.js
    Replace your current index.js with this:
    
    const

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,8 @@
 const functions = require('firebase-functions');
-
-// Logger for debug/info messages
 const logger = functions.logger;
 
 // Example HTTP function
 exports.api = functions.https.onRequest((req, res) => {
   logger.info('Request received', { path: req.path, method: req.method });
-
-  // Example response
   res.send('Hello from Firebase Functions!');
 });
-
-// You can add more functions below, e.g. Firestore triggers
-// exports.myFunction = functions.firestore.document('collection/{docId}').onCreate((snap, context) => {
-//   logger.info('New document added', { id: context.params.docId });
-// });
