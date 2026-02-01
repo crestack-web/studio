6df81d25965a488967c16a59a7b5c5bc745f5634
Crestack Company <crestack@gmail.com>
2026-02-01 10:56:07 +0000

    2️⃣ DELETE EVERYTHING inside it 3️⃣ Paste THIS EXACT CODE (nothing else)

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
-const functions = require("firebase-functions");
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
+exports.helloWorld = onRequest((req, res) => {
+  logger.info("Hello logs!", { structuredData: true });
   res.status(200).send("Functions are working 🚀");
 });
