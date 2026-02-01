f44cc732bbed5cd0a173372fbfe46ada1728db32
Crestack Company <crestack@gmail.com>
2026-02-01 10:44:30 +0000

    Copy-paste this and replace your entire functions/index.js file with it:

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Functions are working 🚀");
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  res.status(200).send("Functions are working 🚀");
 });
