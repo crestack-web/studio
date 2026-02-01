0eaaffa0a2457b0391bdc83e1c89eea804df2503
Crestack Company <crestack@gmail.com>
2026-02-01 11:17:14 +0000

    Here’s a clean version:
    const functions = require('firebase-functions')

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,17 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
 
-exports.helloWorld = onRequest((req, res) => {
-  logger.info("Hello logs!", { structuredData: true });
-  res.status(200).send("Functions are working 🚀");
+// Logger for debug/info messages
+const logger = functions.logger;
+
+// Example HTTP function
+exports.api = functions.https.onRequest((req, res) => {
+  logger.info('Request received', { path: req.path, method: req.method });
+
+  // Example response
+  res.send('Hello from Firebase Functions!');
 });
+
+// You can add more functions below, e.g. Firestore triggers
+// exports.myFunction = functions.firestore.document('collection/{docId}').onCreate((snap, context) => {
+//   logger.info('New document added', { id: context.params.docId });
+// });
