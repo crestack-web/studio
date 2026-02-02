59d726b3f485ccb82ec22bb7b3a455e83b10e938
Crestack Company <crestack@gmail.com>
2026-02-02 00:22:15 +0000

    2️⃣ Go to your project folder C:\Users\sd
    3️⃣ DELETE these folders/file

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require("firebase-functions");
-
-exports.helloWorld = functions.https.onRequest((req, res) => {
-  functions.logger.info("Hello logs!");
-  res.send("Functions are working 🚀");
-});
