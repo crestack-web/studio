f6fa2dae943cac1beacd703fa3ebf88805f971ec
Busmo Team <busmo@busmo.com>
2026-07-21 05:51:47 -0700

    Remove deprecated support chat widgets

--- src/app/welcome/components/SupportChatWidgetWrapper.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/welcome/components/SupportChatWidgetWrapper.tsx
-// Using SupportChatWidget for consistent experience
-'use client';
-
-import SupportChatWidget from '@/components/SupportChatWidget';
-
-export default function SupportChatWidgetWrapper() {
-  return <SupportChatWidget />;
-}
