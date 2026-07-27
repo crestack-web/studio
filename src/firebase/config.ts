3d80a75f1fc91d4733e9008705ded8445c1d9a22
Busmo Team <busmo@busmo.com>
2026-07-27 08:32:38 -0700

    Update firebase config to match Firebase console (correct API key, app ID, measurement ID)

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "bizassistant2-62305643-adad7",
-  "appId": "1:791501057203:web:afeb42948dcd4ac66354c6",
-  "apiKey": "AIzaSyBb_nzjTK6iJ1FlYCJp4oss1Xut0GUubqM",
-  "authDomain": "bizassistant2-62305643-adad7.firebaseapp.com",
-  "measurementId": "G-6GM3N5Z59P",
-  "messagingSenderId": "791501057203",
-  "storageBucket": "bizassistant2-62305643-adad7.firebasestorage.app"
+  apiKey: "AIzaSyA8y0sRbdN5bUeLXf1b-ZYmbGNVaGopT1A",
+  authDomain: "bizassistant2-62305643-adad7.firebaseapp.com",
+  databaseURL: "https://bizassistant2-62305643-adad7-default-rtdb.firebaseio.com",
+  projectId: "bizassistant2-62305643-adad7",
+  storageBucket: "bizassistant2-62305643-adad7.firebasestorage.app",
+  messagingSenderId: "791501057203",
+  appId: "1:791501057203:web:6559ad14fd2d64776354c6",
+  measurementId: "G-5YYNT5JEQQ"
 };
