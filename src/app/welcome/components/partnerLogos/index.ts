ac5262ebd46f4152fff9f52eabdef5f8f5996fd7
business money <crestack@gmail.com>
2026-09-24 03:17:32 -0700

    feat(welcome): export Delizz partner logo

--- src/app/welcome/components/partnerLogos/index.ts
+++ src/app/welcome/components/partnerLogos/index.ts
@@ -1,3 +1,4 @@
 export { LOGO_HILL360 } from "./LOGO_HILL360";
 export { LOGO_CADBURY } from "./LOGO_CADBURY";
 export { LOGO_BRONZE_PLATE } from "./LOGO_BRONZE_PLATE";
+export { LOGO_DELIZZ } from "./LOGO_DELIZZ";
